74ddd42e4a85b4a5e54effb9a56bdbe5d7fe033f
Lamburhts <kayalambrechts@gmail.com>
2025-02-21 16:57:57 +0100

    Update CrateInteractListener.java
    Signed-off-by: Lamburhts <kayalambrechts@gmail.com>

--- paper/src/main/java/com/badbones69/crazycrates/paper/listeners/crates/CrateInteractListener.java
+++ paper/src/main/java/com/badbones69/crazycrates/paper/listeners/crates/CrateInteractListener.java
@@ -18,6 +18,7 @@ import com.badbones69.crazycrates.paper.utils.ItemUtils;
 import com.badbones69.crazycrates.paper.utils.MiscUtils;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.sound.Sound;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -28,174 +29,153 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 import us.crazycrew.crazycrates.api.enums.types.CrateType;
 import us.crazycrew.crazycrates.api.enums.types.KeyType;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class CrateInteractListener implements Listener {
 
     private final CrazyCrates plugin = CrazyCrates.getPlugin();
-
     private final InventoryManager inventoryManager = this.plugin.getInventoryManager();
-
     private final SettingsManager config = ConfigManager.getConfig();
-
     private final CrateManager crateManager = this.plugin.getCrateManager();
-
     private final BukkitUserManager userManager = this.plugin.getUserManager();
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onCrateInteract(final CrateInteractEvent event) {
-        if (event.isCancelled()) return; // do not run this event.
+        if (event.isCancelled()) return; // Do not run this event if it's already cancelled.
 
-        event.cancel(); // cancel the event
+        event.cancel(); // Cancel the event to prevent further processing.
 
         final Player player = event.getPlayer();
-
         final CrateLocation crateLocation = event.getCrateLocation();
         final Crate crate = crateLocation.getCrate();
-
         final Action action = event.getAction();
 
         switch (action) {
             case LEFT_CLICK_BLOCK, LEFT_CLICK_AIR -> {
                 if (crate.getCrateType() == CrateType.menu) {
                     preview(player, crate, true);
-
                     return;
                 }
 
                 final boolean isLeftClickToPreview = this.config.getProperty(ConfigKeys.crate_physical_interaction);
 
-                if (isLeftClickToPreview) { // left click to preview
+                if (isLeftClickToPreview) { // Left click to preview.
                     preview(player, crate, false);
-
                     return;
                 }
 
-                // left click to open
+                // Left click to open.
                 openCrate(player, crateLocation, crate);
             }
 
             case RIGHT_CLICK_BLOCK, RIGHT_CLICK_AIR -> {
                 if (crate.getCrateType() == CrateType.menu) {
                     preview(player, crate, true);
-
                     return;
                 }
 
                 final boolean isRightClickToOpen = this.config.getProperty(ConfigKeys.crate_physical_interaction);
 
-                if (isRightClickToOpen) { // right click to open
+                if (isRightClickToOpen) { // Right click to open.
                     openCrate(player, crateLocation, crate);
-
                     return;
                 }
 
-                // right click to preview
+                // Right click to preview.
                 preview(player, crate, false);
             }
         }
     }
 
-private void openCrate(Player player, CrateLocation crateLocation, Crate crate) {
-    final KeyCheckEvent key = new KeyCheckEvent(player, crateLocation);
-    player.getServer().getPluginManager().callEvent(key);
+    private void openCrate(Player player, CrateLocation crateLocation, Crate crate) {
+        final KeyCheckEvent key = new KeyCheckEvent(player, crateLocation);
+        player.getServer().getPluginManager().callEvent(key);
 
-    if (key.isCancelled()) return;
+        if (key.isCancelled()) return;
 
-    final Location location = crateLocation.getLocation();
+        final Location location = crateLocation.getLocation();
 
-    boolean hasKey = false;
-    boolean isPhysical = false;
-    boolean useQuickCrateAgain = false;
+        boolean hasKey = false;
+        boolean isPhysical = false;
+        boolean useQuickCrateAgain = false;
 
-    final int requiredKeys = crate.getRequiredKeys();
+        final int requiredKeys = crate.getRequiredKeys();
+        final String fileName = crate.getFileName();
+        final int totalKeys = this.userManager.getTotalKeys(player.getUniqueId(), fileName);
+        final String fancyName = crate.getCrateName();
 
-    final String fileName = crate.getFileName();
+        if (requiredKeys > 0 && totalKeys < requiredKeys) {
+            final Map<String, String> placeholders = new HashMap<>();
+            placeholders.put("{required_amount}", String.valueOf(requiredKeys));
+            placeholders.put("{key_amount}", String.valueOf(requiredKeys)); // Deprecated, remove in next major version of Minecraft.
+            placeholders.put("{amount}", String.valueOf(totalKeys));
+            placeholders.put("{crate}", fancyName);
+            placeholders.put("{key}", crate.getKeyName());
 
-    final int totalKeys = this.userManager.getTotalKeys(player.getUniqueId(), fileName);
+            Messages.not_enough_keys.sendMessage(player, placeholders);
+            lackingKey(player, crate, location, false);
 
-    final String fancyName = crate.getCrateName();
+            key.setCancelled(true);
+            return;
+        }
 
-    if (requiredKeys > 0 && totalKeys < requiredKeys) {
-        final Map<String, String> placeholders = new HashMap<>();
+        final ItemStack itemStack = player.getInventory().getItemInMainHand();
 
-        placeholders.put("{required_amount}", String.valueOf(requiredKeys));
-        placeholders.put("{key_amount}", String.valueOf(requiredKeys)); // deprecated, remove in next major version of minecraft.
-        placeholders.put("{amount}", String.valueOf(totalKeys));
-        placeholders.put("{crate}", fancyName);
-        placeholders.put("{key}", crate.getKeyName());
+        if (this.config.getProperty(ConfigKeys.physical_accepts_physical_keys) && crate.getCrateType() != CrateType.crate_on_the_go && ItemUtils.isSimilar(itemStack, crate)) {
+            hasKey = true;
+            isPhysical = true;
+        } else if (this.config.getProperty(ConfigKeys.physical_accepts_virtual_keys) && this.userManager.getVirtualKeys(player.getUniqueId(), fileName) >= 1) {
+            hasKey = true;
+        }
 
-        Messages.not_enough_keys.sendMessage(player, placeholders);
+        if (hasKey) {
+            // Check if the player uses the quick crate again.
+            if (this.crateManager.isInOpeningList(player) && this.crateManager.getOpeningCrate(player).getCrateType() == CrateType.quick_crate && this.crateManager.isCrateInUse(player) && this.crateManager.getCrateInUseLocation(player).equals(crateLocation.getLocation())) {
+                useQuickCrateAgain = true;
+            }
 
-        lackingKey(player, crate, location, false);
+            if (!useQuickCrateAgain) {
+                if (this.crateManager.isInOpeningList(player)) {
+                    Messages.already_opening_crate.sendMessage(player, "{crate}", fancyName);
+                    return;
+                }
 
+                if (this.crateManager.getCratesInUse().containsValue(crateLocation.getLocation())) {
+                    Messages.crate_in_use.sendMessage(player, "{crate}", fancyName);
+                    return;
+                }
+            }
+
+            if (MiscUtils.isInventoryFull(player)) {
+                Messages.inventory_not_empty.sendMessage(player, "{crate}", fancyName);
+                return;
+            }
+
+            if (useQuickCrateAgain) this.crateManager.endQuickCrate(player, crateLocation.getLocation(), crate, true);
+
+            final KeyType keyType = isPhysical ? KeyType.physical_key : KeyType.virtual_key;
+
+            // Only cosmic crate type uses this method.
+            if (crate.getCrateType() == CrateType.cosmic) this.crateManager.addPlayerKeyType(player, keyType);
+
+            this.crateManager.addPlayerToOpeningList(player, crate);
+
+            // Call pickPrize and handle null case.
+            Prize prize = crate.pickPrize(player);
+            if (prize == null) {
+                player.sendMessage(ChatColor.RED + "You do not have permission to win any prizes from this crate.");
+                this.crateManager.removePlayerFromOpeningList(player);
+                return;
+            }
+
+            this.crateManager.openCrate(player, crate, keyType, location, false, true, EventType.event_crate_opened);
+            return;
+        }
+
+        lackingKey(player, crate, location, true);
         key.setCancelled(true);
-
-        return;
-    }
-
-    final ItemStack itemStack = player.getInventory().getItemInMainHand();
-
-    if (this.config.getProperty(ConfigKeys.physical_accepts_physical_keys) && crate.getCrateType() != CrateType.crate_on_the_go && ItemUtils.isSimilar(itemStack, crate)) {
-        hasKey = true;
-        isPhysical = true;
-    } else if (this.config.getProperty(ConfigKeys.physical_accepts_virtual_keys) && this.userManager.getVirtualKeys(player.getUniqueId(), fileName) >= 1) {
-        hasKey = true;
-    }
-
-    if (hasKey) {
-        // Checks if the player uses the quick crate again.
-        if (this.crateManager.isInOpeningList(player) && this.crateManager.getOpeningCrate(player).getCrateType() == CrateType.quick_crate && this.crateManager.isCrateInUse(player) && this.crateManager.getCrateInUseLocation(player).equals(crateLocation.getLocation())) {
-            useQuickCrateAgain = true;
-        }
-
-        if (!useQuickCrateAgain) {
-            if (this.crateManager.isInOpeningList(player)) {
-                Messages.already_opening_crate.sendMessage(player, "{crate}", fancyName);
-
-                return;
-            }
-
-            if (this.crateManager.getCratesInUse().containsValue(crateLocation.getLocation())) {
-                Messages.crate_in_use.sendMessage(player, "{crate}", fancyName);
-
-                return;
-            }
-        }
-
-        if (MiscUtils.isInventoryFull(player)) {
-            Messages.inventory_not_empty.sendMessage(player, "{crate}", fancyName);
-
-            return;
-        }
-
-        if (useQuickCrateAgain) this.crateManager.endQuickCrate(player, crateLocation.getLocation(), crate, true);
-
-        final KeyType keyType = isPhysical ? KeyType.physical_key : KeyType.virtual_key;
-
-        // Only cosmic crate type uses this method.
-        if (crate.getCrateType() == CrateType.cosmic) this.crateManager.addPlayerKeyType(player, keyType);
-
-        this.crateManager.addPlayerToOpeningList(player, crate);
-
-        // Call pickPrize and handle null case
-        Prize prize = crate.pickPrize(player);
-        if (prize == null) {
-            player.sendMessage(ChatColor.RED + "You do not have permission to win any prizes from this crate.");
-            this.crateManager.removePlayerFromOpeningList(player);
-            return;
-        }
-
-        this.crateManager.openCrate(player, crate, keyType, location, false, true, EventType.event_crate_opened);
-
-        return;
-    }
-
-    lackingKey(player, crate, location, true);
-
-    key.setCancelled(true);
-}
     }
 
     private void lackingKey(final Player player, final Crate crate, final Location location, final boolean sendMessage) {
@@ -211,7 +191,6 @@ private void openCrate(Player player, CrateLocation crateLocation, Crate crate) 
 
             if (this.config.getProperty(ConfigKeys.need_key_sound_toggle)) {
                 net.kyori.adventure.sound.Sound sound = net.kyori.adventure.sound.Sound.sound(Key.key(this.config.getProperty(ConfigKeys.need_key_sound)), Sound.Source.MASTER, 1f, 1f);
-
                 player.playSound(sound);
             }
 
@@ -224,7 +203,6 @@ private void openCrate(Player player, CrateLocation crateLocation, Crate crate) 
 
         if (player.isInsideVehicle() && player.getVehicle() != null) {
             player.getVehicle().setVelocity(vector);
-
             return;
         }
 
@@ -233,7 +211,7 @@ private void openCrate(Player player, CrateLocation crateLocation, Crate crate) 
 
     private void preview(final Player player, final Crate crate, boolean skipTypeCheck) {
         if (skipTypeCheck || crate.getCrateType() == CrateType.menu) {
-            // this is to stop players in QuadCrate to not be able to try and open a crate set to menu.
+            // This is to stop players in QuadCrate from trying to open a crate set to menu.
             if (!this.crateManager.isInOpeningList(player) && this.config.getProperty(ConfigKeys.enable_crate_menu)) {
                 new CrateMainMenu(
                         player,
